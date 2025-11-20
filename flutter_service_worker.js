334458dc613e30a14aa53a03aae22bc1d83035ca
chatgptkrguide <chatgptkrguide@users.noreply.github.com>
2025-11-20 15:42:25 +0000

    deploy: e4badf0212d57edcdaf179ccd31e1ec66ab0008a

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -76,10 +76,10 @@ const RESOURCES = {"flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "canvaskit/canvaskit.js.symbols": "efc2cd87d1ff6c586b7d4c7083063a40",
 "canvaskit/skwasm.js.symbols": "96263e00e3c9bd9cd878ead867c04f3c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "8c8aad174c2596ffec8e00eed987912f",
+"flutter_bootstrap.js": "49ff0a6c5d542fc8d346434a380a7daf",
 "index.html.backup": "5784b4f1893044f890afc4845ba7c2e5",
 "version.json": "1e4564b09609c1b0c25549827e09be4d",
-"main.dart.js": "ae5a74be9b83744a6c222c48547e7320"};
+"main.dart.js": "d38f927d079607d28c56a04cfe5d72e0"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
