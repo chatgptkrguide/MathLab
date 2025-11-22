6b1448b4ca72023e52a963a649d79e9aa13347c8
chatgptkrguide <chatgptkrguide@users.noreply.github.com>
2025-11-22 10:22:47 +0000

    deploy: 23f10b9d52331387b3be7bc6d3329619a93dd21f

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -82,7 +82,7 @@ const RESOURCES = {"flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "canvaskit/canvaskit.js.symbols": "efc2cd87d1ff6c586b7d4c7083063a40",
 "canvaskit/skwasm.js.symbols": "96263e00e3c9bd9cd878ead867c04f3c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "189ac6584c6344dede77a8910c283775",
+"flutter_bootstrap.js": "addbb7a47a4b9532199815a72fc86fa5",
 "index.html.backup": "5784b4f1893044f890afc4845ba7c2e5",
 "version.json": "1e4564b09609c1b0c25549827e09be4d",
 "main.dart.js": "cc8027b4e47096ba690f24bb88836050"};
