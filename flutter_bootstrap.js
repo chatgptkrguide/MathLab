774055c99fc5f528804e8280ee0a9d58cfcd64ad
chatgptkrguide <chatgptkrguide@users.noreply.github.com>
2025-11-25 00:58:45 +0000

    deploy: 04516a678ec8e17baee4705022f0cad1559ccea7

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "9339848"
+    serviceWorkerVersion: "1486475121"
   }
 });
